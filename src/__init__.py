45556cbb9589c7af647b2645b5e7b7eb53535a38
saeedsabalandve <saeedsabalandve@gmail.com>
2026-06-23 09:05:31 +0330

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+# src/__init__.py
+# Main source package initialization
+# Core microservice for resource monitoring and prediction
+# Version: 1.0.0
+
+__version__ = "1.0.0"
+__author__ = "Your Team"
+__description__ = "Resource Monitor & Predictor Microservice"
